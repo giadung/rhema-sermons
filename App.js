07e13e51405e9fe92f14b8874f63835c4cbeeac2
Roxanne <tran.giadung@gmail.com>
2021-03-14 19:04:03 -0400

    fixed VideoScreen layout

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import BottomNavigation, {
+  FullTab
+} from 'react-native-material-bottom-navigation'
 import SermonsScreen from './src/screens/SermonsScreen'
 import VideoScreen from './src/screens/VideoScreen'
 
